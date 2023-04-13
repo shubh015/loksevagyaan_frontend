fdab5115af91d0bfa4253cec07114758fb74b954
Shubham Singh <shubhamsingh@webllisto.com>
2023-04-13 18:40:32 +0530

    solve bug

--- src/containers/CurrentAffairsContainer/CurrentModal.tsx
+++ src/containers/CurrentAffairsContainer/CurrentModal.tsx
@@ -11,70 +11,68 @@ import {
 } from "@material-tailwind/react";
 
 export default function CurrentModal({ isOpen, setIsOpen }: any) {
-    const [open, setOpen] = useState(0);
- 
-    const handleOpen = (value:any) => {
-      setOpen(open === value ? 0 : value);
-    };
-   
-    const customAnimation = {
-      mount: { scale: 1 },
-      unmount: { scale: 0.9 },
-    };
-   
- 
+  const [open, setOpen] = useState(0);
+
+  const handleOpen = (value: any): void => {
+    setOpen(open === value ? 0 : value);
+  };
+
+  const customAnimation = {
+    mount: { scale: 1 },
+    unmount: { scale: 0.9 },
+  };
 
   return (
     <Fragment>
       <Dialog open={isOpen} size={"xxl"} handler={handleOpen}>
         <DialogHeader>Its a simple dialog.</DialogHeader>
         <DialogBody divider>
-        <Fragment>
-      <Accordion open={open === 1} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(1)}>
-          What is Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(2)}>
-          How to use Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(3)}>
-          What can I do with Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-    </Fragment>
+          <Fragment>
+            <Accordion open={open === 1} animate={customAnimation}>
+              <AccordionHeader onClick={() => handleOpen(1)}>
+                What is Material Tailwind?
+              </AccordionHeader>
+              <AccordionBody>
+                We&apos;re not always in the position that we want to be at.
+                We&apos;re constantly growing. We&apos;re constantly making
+                mistakes. We&apos;re constantly trying to express ourselves and
+                actualize our dreams.
+              </AccordionBody>
+            </Accordion>
+            <Accordion open={open === 2} animate={customAnimation}>
+              <AccordionHeader onClick={() => handleOpen(2)}>
+                How to use Material Tailwind?
+              </AccordionHeader>
+              <AccordionBody>
+                We&apos;re not always in the position that we want to be at.
+                We&apos;re constantly growing. We&apos;re constantly making
+                mistakes. We&apos;re constantly trying to express ourselves and
+                actualize our dreams.
+              </AccordionBody>
+            </Accordion>
+            <Accordion open={open === 3} animate={customAnimation}>
+              <AccordionHeader onClick={() => handleOpen(3)}>
+                What can I do with Material Tailwind?
+              </AccordionHeader>
+              <AccordionBody>
+                We&apos;re not always in the position that we want to be at.
+                We&apos;re constantly growing. We&apos;re constantly making
+                mistakes. We&apos;re constantly trying to express ourselves and
+                actualize our dreams.
+              </AccordionBody>
+            </Accordion>
+          </Fragment>
         </DialogBody>
         <DialogFooter>
           <Button
             variant="text"
             color="red"
-            onClick={() => handleOpen()}
+            onClick={() => handleOpen("")}
             className="mr-1"
           >
             <span>Cancel</span>
           </Button>
-          <Button variant="gradient" color="green" onClick={() => handleOpen()}>
+          <Button variant="gradient" color="green" onClick={() => handleOpen("")}>
             <span>Confirm</span>
           </Button>
         </DialogFooter>
